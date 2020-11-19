0f1a5edee9a6af8a46138156854ac8ddef16617f
Pietro Henrique <pietro_henrique4@hotmail.com>
2020-11-18 23:58:36 -0300

    .gitignore arrumado

--- dist/server.js
+++ dist/server.js
@@ -8,13 +8,6 @@ var port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //Implementação do servidor para front-end de produção
-app.get('/api/hello', function (req, res) {
-    res.send({ express: 'Hello From Express' });
-});
-app.post('/api/world', function (req, res) {
-    console.log(req.body);
-    res.send("I received your POST request. This is what you sent me: " + req.body.post);
-});
 app.post('/api/login', function (req, res) {
     //Fazer Authorização utilizando o  banco de dados 
     console.log(req.body);
