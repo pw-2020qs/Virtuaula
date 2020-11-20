68d385c4bb974cc5478dc4e079f68f36efe20c62
Pietro Henrique <pietro_henrique4@hotmail.com>
2020-11-19 21:17:02 -0300

    Roteador do React foi passado para App.ts, ao invés de index.t

---
+++ client/build/precache-manifest.9590464de76551f73418402991f6a147.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9431e405e3345342496c9a6b7e968b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23af0c0e557f6b0940ef",
+    "url": "/static/css/main.d7b8d30e.chunk.css"
+  },
+  {
+    "revision": "bd88c0607e688bce766c",
+    "url": "/static/js/2.6bc98bc5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6bc98bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23af0c0e557f6b0940ef",
+    "url": "/static/js/main.0680f667.chunk.js"
+  },
+  {
+    "revision": "9e87fa2506ae9f15adda",
+    "url": "/static/js/runtime-main.80deff1e.js"
+  },
+  {
+    "revision": "4e8863dccf12d313d984b1c80729c022",
+    "url": "/static/media/logo512.4e8863dc.jpeg"
+  },
+  {
+    "revision": "f520e79f7eb83d5f16f92adcd6cb4e5b",
+    "url": "/static/media/logo512.f520e79f.png"
+  },
+  {
+    "revision": "66d5f486932524dcd47759b6ab6e1ff3",
+    "url": "/static/media/logo_small.66d5f486.png"
+  }
+]);
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,27 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Landing from './pages/landing/landing';
-import Perfil from './pages/perfil/perfil';
-import logIn from './pages/logIn/logIn';
-import Cadastro from './pages/cadastro/cadastro';
 
 
 ReactDOM.render(
   <React.StrictMode>
-
-    <Router>
-      <Switch>
-      <Route path="/" exact component={Landing}/>
-      <Route path="/logIn" exact component={logIn}/>
-      <Route path="/perfil" exact component={Perfil}/>
-      <Route path="/logIn/Cadastro" exact component={Cadastro} />
-      </Switch>
-
-    </Router>
-
-
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
